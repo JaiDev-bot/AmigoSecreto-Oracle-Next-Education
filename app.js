ce2beb4ade781d7362b73a83ba9c21d78818ce7a
Jaiane <jaianesoares700@gmail.com>
2025-08-10 09:58:12 -0300

    feat: adiciona função adicionar

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo');
+    if (amigo.value == '') {
+        alert('Informe o nome do amigo!');
+        return;
+    }
+
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado!');
+        return;
+    }
+
+    let lista = document.getElementById('lista-amigos');
+    amigos.push(amigo.value);
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+
+
+
+}
