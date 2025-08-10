398c1e97bac39dd572fad2460086cbdcda6da280
Jaiane <jaianesoares700@gmail.com>
2025-08-10 12:45:22 -0300

    feat: adiciona função sortearAmigo e embaralha

--- app.js
+++ app.js
@@ -22,5 +22,35 @@ function adicionarAmigo() {
     amigo.value = '';
 
 
-
 }
+
+function sortearAmigo() {
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos!');
+        return;
+    }
+    embaralha(amigos);
+    
+    let sorteio = document.getElementById('resultado');
+    sorteio.innerHTML = ''; 
+    
+    for (let i = 0; i < amigos.length; i++) {
+        const nomeAtual = amigos[i];
+        const nomeSorteado = (i == amigos.length - 1) ? amigos[0] : amigos[i + 1];
+        
+        sorteio.innerHTML += `<span>${nomeAtual}</span> --> <span>${nomeSorteado}</span><br>`;
+    }
+}
+    
+
+
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+        [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+
+
